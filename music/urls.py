abe8442fe4f869ae23fb9111304d9856bcd1f609
Alim03 <ali.mohamadkhani03@gmail.com>
2022-12-27 20:51:02 +0330

    Index, Album, Song

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.Index.as_view(), name='index'),
+    path('play/<int:pk>', views.Play.as_view(), name='play'),
+    path('album/<int:pk>', views.AlbumDetail.as_view(), name='album-detail')
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView, DetailView
+from .models import Artist, Song, Album
 
 
-def index(request):
-    return render(request, "music/index.html")
+class Index(ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'artists'
+    model = Artist
+
+    def get_queryset(self):
+        query = super().get_queryset()
+        return query[:3]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['songs'] = Song.objects.all().order_by('-id')[:3]
+        context['albums'] = Album.objects.all().order_by('-id')[:3]
+        return context
+
+
+class Play(DetailView):
+    template_name = 'music/player.html'
+    model = Song
+
+
+class AlbumDetail(DetailView):
+    template_name = 'music/album_detail.html'
+    context_object_name='album'
+    model = Album
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        id = self.kwargs['pk']
+        album=Album.objects.get(pk=id)
+        context["album_songs"] = album.song_set.all()
+        return context
